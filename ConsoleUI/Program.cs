5181b83db3a2ec3db29ddd2dd1bdfa315ff6538e
Ali Sencer Irmak <alisencer.irmak@std.yeditepe.edu.tr>
2024-07-30 20:54:03 +0300

    ef giris

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -4,16 +4,35 @@ using DataAccess.Concrete.InMemory;
 using System;
 
 namespace ConsoleUI
-{
+{  
     public class Program
     {
         static void Main(string[] args)
         {
+            ProductTest();
+
+            //CategoryTest();
+
+
+
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var item in categoryManager.GetAll())
+            {
+                Console.WriteLine(item.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var item in productManager.GetAllByUnitPrice(50,100))
+            foreach (var item in productManager.GetProductDetails())
             {
-                Console.WriteLine(item.ProductName);
+                Console.WriteLine(item.ProductName + "--" + item.CategoryName);
             }
         }
     }
