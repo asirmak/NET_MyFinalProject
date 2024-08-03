6b3e23d66713319781e717dbd70366b26621d5ea
Ali Sencer Irmak <alisencer.irmak@std.yeditepe.edu.tr>
2024-08-03 13:15:58 +0300

    jwt appsetting json core entities

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -29,7 +29,7 @@ namespace Business.Concrete
             _productDal = productDal;
             _categoryService = categoryService;
         }
-
+         
         [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
